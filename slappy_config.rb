2398afe3c6e8c938a7b3a45203c61309ba41e12d
myamamoto88 <myamamoto@aiming-inc.com>
2017-06-11 23:25:15 +0900

    Add bot status to config

--- slappy_config.rb
+++ slappy_config.rb
@@ -4,9 +4,8 @@ require 'choice'
 
 Slappy.configure do |config|
   config.token = ENV['BOT_TOKEN']
-  # config.robot.username = 'slappy'
-  # config.robot.icon_url = nil
-
+  config.robot.username = 'チョイス'
+  config.robot.icon_url = 'https://user-images.githubusercontent.com/7444925/27010947-84e8c1c6-4eeb-11e7-8d82-41ba6322d1a0.png'
 
   # debug
   #config.logger = Logger.new(STDOUT)
