c43450385a0d1665df1f64e968fa65adeaa032eb
myamamoto88 <myamamoto@aiming-inc.com>
2017-06-11 18:45:57 +0900

    Fix the issue that raise error when group not found

--- lib/choice/command/show.rb
+++ lib/choice/command/show.rb
@@ -4,10 +4,14 @@ module Choice
       private
 
       def members
-        JSON.parse(store.get(member_key))
+        _members = store.get(member_key)
+        return [] if _members.blank?
+        JSON.parse(_members)
       end
 
       def message
+        return "「#{group_name}」は登録されていません。" if members.empty?
+
         <<-EOS
 グループ名: #{group_name}
 メンバー: #{members.join(', ')}
