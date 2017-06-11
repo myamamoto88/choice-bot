bbc086e3dd96d19bb275bdb810cfebce828de4db
myamamoto88 <myamamoto@aiming-inc.com>
2017-06-11 20:42:25 +0900

    Add shuffle command

---
+++ lib/choice/command/shuffle.rb
@@ -0,0 +1,36 @@
+module Choice
+  module Command
+    class Shuffle < Base
+      private
+
+      def action
+        store.set(member_key, after_members)
+        store.set(cursor_key, 0)
+      end
+
+      def before_members
+        @before_members ||= begin
+          _members = store.get(member_key)
+          if _members.blank?
+            []
+          else
+            JSON.parse(_members)
+          end
+        end
+      end
+
+      def after_members
+        @after_members ||= before_members.shuffle
+      end
+
+      def message
+        <<-EOS
+「#{group_name}」をシャッフルしました。
+
+前: #{before_members.join(', ')}
+後: #{after_members.join(', ')}
+        EOS
+      end
+    end
+  end
+end
